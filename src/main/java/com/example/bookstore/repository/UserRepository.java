2df5df29c2b26cb215e224e9215558d4aef00a6b
ThenScu <bimapbeo1001@gmail.com>
2026-03-10 14:18:04 +0700

    Them logic login register

---
+++ src/main/java/com/example/bookstore/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Báo cho Spring Boot biết đây là Thợ mỏ đào dữ liệu
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Ảo ma chưa: Chỉ cần gõ đúng tên hàm, Spring Boot TỰ ĐỘNG sinh ra câu lệnh SQL:
+    // SELECT * FROM users WHERE username = ?
+    User findByUsername(String username);
+
+    // Tự động kiểm tra xem username đã có trong DB chưa
+    boolean existsByUsername(String username);
+}

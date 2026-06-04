f6eddd833bc1919a0b4f4dee10a6226bc442cddf
Thèn Scu <bimapbeo1001@gmail.com>
2026-06-04 17:07:55 +0700

    507

--- src/main/java/com/example/bookstore/distributed/DistributedLock.java
+++ src/main/java/com/example/bookstore/distributed/DistributedLock.java
@@ -17,12 +17,14 @@ public class DistributedLock {
     @Column(name = "lock_holder_id", length = 255, nullable = false)
     private String lockHolderId = "UNOWNED";
 
+//    @Column(name = "lock_expires_at", nullable = false)
+//    private LocalDateTime lockExpiresAt = LocalDateTime.now();
+
     @Column(name = "lock_expires_at", nullable = false)
     private LocalDateTime lockExpiresAt = LocalDateTime.now();
 
-    // [1] THÊM BIẾN ACQUIRED_AT VÀO ĐÂY
-    @Column(name = "acquired_at", nullable = false)
-    private LocalDateTime acquiredAt = LocalDateTime.now();
+    @Column(name = "acquired_at", nullable = false, columnDefinition = "datetime2(6) DEFAULT SYSUTCDATETIME()")
+    private LocalDateTime acquiredAt;
 
     // Constructors
     public DistributedLock() {}
